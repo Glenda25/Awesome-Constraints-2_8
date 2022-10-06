04c9b0933e16fe42414a4662ab01c8303d361ec1
Glenda Mullan <glenda.mullan@gmail.com>
2022-10-06 14:00:13 +1100

    Step 1, layout

--- Awesome Constraints 2_8/ViewController.swift
+++ Awesome Constraints 2_8/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func showMessage(_ sender: UIButton) {
+    }
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
 }
 
