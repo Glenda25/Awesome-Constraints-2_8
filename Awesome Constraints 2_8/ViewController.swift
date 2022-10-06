351845c12f45697cfaaf4083dc71d844ec998d74
Glenda Mullan <glenda.mullan@gmail.com>
2022-10-06 14:17:34 +1100

    Step 2

--- Awesome Constraints 2_8/ViewController.swift
+++ Awesome Constraints 2_8/ViewController.swift
@@ -10,14 +10,20 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var messageLabel: UILabel!
+    //don't put anything here
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("View did load")
+        //start up message in label
+        messageLabel.text = ""
+    
     }
 
-
     @IBAction func showMessage(_ sender: UIButton) {
+        print("showMessage button pushed")
+        messageLabel.text = "Diving is awesome"
+        imageView.image = UIImage(named: "image1")
     }
     
     
